ac4983f90c7403032b9973d2562093cbde69ec68
maddymakesgames <madelinemakesgames@gmail.com>
2022-11-23 15:22:20 -0500

    Fix Dijkstra's and implement A*

--- src/traversers.rs
+++ src/traversers.rs
@@ -1,5 +1,7 @@
 use std::collections::HashSet;
 
+use egui::Pos2;
+
 use crate::graph::{Graph, NodeIndex};
 
 pub struct TraversalData {
@@ -123,7 +125,9 @@ impl TraversalData {
                         let node_to_visit = graph.get_node_mut(a);
 
                         node_to_visit.view();
-                        if node_to_visit.get_curr_path() > path_len + weight {
+                        if node_to_visit.get_curr_path() > path_len + weight
+                            || node_to_visit.get_last_node().is_none()
+                        {
                             node_to_visit.set_last_node(idx, path_len + weight);
                         }
 
@@ -135,7 +139,9 @@ impl TraversalData {
                     let node_to_visit = graph.get_node_mut(b);
 
                     node_to_visit.view();
-                    if node_to_visit.get_curr_path() > path_len + weight {
+                    if node_to_visit.get_curr_path() > path_len + weight
+                        || node_to_visit.get_last_node().is_none()
+                    {
                         node_to_visit.set_last_node(idx, path_len + weight);
                     }
 
@@ -152,8 +158,73 @@ impl TraversalData {
         self.to_traverse.is_empty()
     }
 
-    pub fn astar_step(&mut self, _graph: &mut Graph) -> bool {
-        true
+    pub fn astar_step(&mut self, graph: &mut Graph) -> bool {
+        let (priority, idx) = self.to_traverse.remove(0);
+        let node = graph.get_node_mut(idx);
+
+        if self.visited.contains(&idx) {
+            return self.to_traverse.is_empty();
+        }
+
+        node.visit();
+        self.visited.insert(idx);
+
+        let path_len = node.get_curr_path();
+
+        if idx == self.end_node {
+            node.end();
+            return self.to_traverse.is_empty();
+        }
+
+        let node_pos = Pos2::from(node.get_pos());
+
+        let neighbors = node
+            .get_edges()
+            .iter()
+            .filter_map(|e| {
+                let (weight, a, b) = e.get_weighted_nodes();
+                if a != idx {
+                    if self.visited.contains(&a) {
+                        None
+                    } else {
+                        let node_to_visit = graph.get_node_mut(a);
+
+                        let distance = node_pos.distance(node_to_visit.get_pos().into());
+
+                        node_to_visit.view();
+                        if node_to_visit.get_curr_path() > path_len + weight
+                            || node_to_visit.get_last_node().is_none()
+                        {
+                            node_to_visit.set_last_node(idx, path_len + weight);
+                        }
+
+                        Some((priority + distance + weight, a))
+                    }
+                } else if self.visited.contains(&b) {
+                    None
+                } else {
+                    let node_to_visit = graph.get_node_mut(b);
+
+                    let distance = node_pos.distance(node_to_visit.get_pos().into());
+
+                    node_to_visit.view();
+                    if node_to_visit.get_curr_path() > path_len + weight
+                        || node_to_visit.get_last_node().is_none()
+                    {
+                        node_to_visit.set_last_node(idx, path_len + weight);
+                    }
+
+                    Some((priority + distance + weight, b))
+                }
+            })
+            .collect::<Vec<_>>();
+
+        self.to_traverse.extend(neighbors.iter());
+
+        self.to_traverse
+            .sort_by(|(p1, _), (p2, _)| p1.partial_cmp(p2).unwrap());
+
+        self.to_traverse.is_empty()
     }
 
     pub fn new(start_node: NodeIndex, end_node: NodeIndex, alg: GraphTraversers) -> TraversalData {
