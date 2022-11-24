41838d9c7f8925ca537b822bf706221422380a1c
maddymakesgames <madelinemakesgames@gmail.com>
2022-11-24 11:54:48 -0600

    Make the painter settings menu look decent

--- src/menus/painter.rs
+++ src/menus/painter.rs
@@ -11,97 +11,82 @@ impl Menu for GraphPainterMenu {
         ui.heading("Node Settings");
         ui.separator();
         Grid::new("node settings").show(ui, |ui| {
-            ui.horizontal(|ui| {
-                ui.label("Node Color");
-                ui.color_edit_button_srgba(&mut painter.node_color);
-            });
+            ui.label("Node Color");
+            ui.color_edit_button_srgba(&mut painter.node_color);
 
-            ui.horizontal(|ui| {
-                ui.label("Start Node Color");
-                ui.color_edit_button_srgba(&mut painter.start_color);
-            });
+            ui.add_space(10.0);
+
+            ui.label("Start Node Color");
+            ui.color_edit_button_srgba(&mut painter.start_color);
 
             ui.end_row();
 
-            ui.horizontal(|ui| {
-                ui.label("End Node Color");
-                ui.color_edit_button_srgba(&mut painter.end_node_color);
-            });
+            ui.label("End Node Color");
+            ui.color_edit_button_srgba(&mut painter.end_node_color);
 
-            ui.horizontal(|ui| {
-                ui.label("Seen Node Color");
-                ui.color_edit_button_srgba(&mut painter.seen_color);
-            });
+            ui.add_space(10.0);
+
+            ui.label("Seen Node Color");
+            ui.color_edit_button_srgba(&mut painter.seen_color);
 
             ui.end_row();
 
-            ui.horizontal(|ui| {
-                ui.label("Visited Node Color");
-                ui.color_edit_button_srgba(&mut painter.visited_color);
-            });
+            ui.label("Visited Node Color");
+            ui.color_edit_button_srgba(&mut painter.visited_color);
 
             ui.end_row();
 
-            ui.horizontal(|ui| {
-                ui.label("Node Radius");
-                DragValue::new(&mut painter.node_radius).ui(ui);
-            });
-            ui.horizontal(|ui| {
-                ui.label("Node Stroke");
-                DragValue::new(&mut painter.node_stroke).ui(ui);
-            });
+            ui.label("Node Radius");
+            DragValue::new(&mut painter.node_radius).ui(ui);
+
+            ui.add_space(10.0);
+
+            ui.label("Node Stroke");
+            DragValue::new(&mut painter.node_stroke).ui(ui);
 
             ui.end_row();
 
-            ui.horizontal(|ui| {
-                ui.label("Node Text Color");
-                ui.color_edit_button_srgba(&mut painter.node_text_color);
-            });
+            ui.label("Node Text Color");
+            ui.color_edit_button_srgba(&mut painter.node_text_color);
 
-            ui.horizontal(|ui| {
-                ui.label("Node Text Size");
-                DragValue::new(&mut painter.node_text_size)
-                    .clamp_range(1..=100)
-                    .ui(ui);
-            });
+            ui.add_space(10.0);
+
+            ui.label("Node Text Size");
+            DragValue::new(&mut painter.node_text_size)
+                .clamp_range(1..=100)
+                .ui(ui);
         });
 
         ui.heading("Edge Settings");
         ui.separator();
         Grid::new("edge settings").show(ui, |ui| {
-            ui.horizontal(|ui| {
-                ui.label("Edge Color");
-                ui.color_edit_button_srgba(&mut painter.edge_color);
-            });
+            ui.label("Edge Color");
+            ui.color_edit_button_srgba(&mut painter.edge_color);
 
-            ui.horizontal(|ui| {
-                ui.label("Edge Stroke");
-                DragValue::new(&mut painter.edge_stroke).ui(ui);
-            });
+            ui.add_space(10.0);
+
+            ui.label("Edge Stroke");
+            DragValue::new(&mut painter.edge_stroke).ui(ui);
 
             ui.end_row();
 
-            ui.horizontal(|ui| {
-                ui.label("Path Color");
-                ui.color_edit_button_srgba(&mut painter.path_color);
-            });
+            ui.label("Path Color");
+            ui.color_edit_button_srgba(&mut painter.path_color);
 
             ui.end_row();
 
-            ui.horizontal(|ui| {
-                ui.label("Arrow Length");
-                DragValue::new(&mut painter.arrow_length)
-                    .clamp_range(0..=100)
-                    .ui(ui);
-            });
+            ui.label("Arrow Length");
+            DragValue::new(&mut painter.arrow_length)
+                .clamp_range(0..=100)
+                .ui(ui);
 
-            ui.horizontal(|ui| {
-                ui.label("Curved Arrow Angle");
-                DragValue::new(&mut painter.curved_arrow_angle)
-                    .clamp_range(0.0..=std::f32::consts::FRAC_PI_2)
-                    .speed(0.01)
-                    .ui(ui);
-            });
+            ui.add_space(10.0);
+
+            ui.label("Curved Arrow Angle");
+            DragValue::new(&mut painter.curved_arrow_angle)
+                .clamp_range(0.0..=std::f32::consts::FRAC_PI_2)
+                .speed(0.01)
+                .ui(ui);
 
             ui.end_row();
         });
@@ -109,24 +94,18 @@ impl Menu for GraphPainterMenu {
         ui.heading("Weight Settings");
         ui.separator();
         Grid::new("Weight settings").show(ui, |ui| {
-            ui.horizontal(|ui| {
-                ui.label("Weight Text Color");
-                ui.color_edit_button_srgba(&mut painter.weight_text_color);
-            });
+            ui.label("Weight Text Color");
+            ui.color_edit_button_srgba(&mut painter.weight_text_color);
 
-            ui.horizontal(|ui| {
-                ui.label("Weight Text Size");
-                DragValue::new(&mut painter.weight_text_size)
-                    .clamp_range(1..=100)
-                    .ui(ui);
-            });
+            ui.label("Weight Text Size");
+            DragValue::new(&mut painter.weight_text_size)
+                .clamp_range(1..=100)
+                .ui(ui);
 
             ui.end_row();
 
-            ui.horizontal(|ui| {
-                ui.label("Text Color Background");
-                ui.color_edit_button_srgba(&mut painter.text_background_color);
-            });
+            ui.label("Text Color Background");
+            ui.color_edit_button_srgba(&mut painter.text_background_color);
         });
     }
 
